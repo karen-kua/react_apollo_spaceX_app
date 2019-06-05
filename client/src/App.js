3105d9638f8b965f818e3c04f7ef26421c440b30
Durian <karen.kua7@gmail.com>
2019-06-05 00:53:02 -0400

    Uploading working files for react apollo app

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Launches from "./components/Launches";
+import Launch from "./components/Launch";
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql'
+})
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <Router>
+          <div className="container">
+            <h1>Space X</h1>
+            <Route exact path='/' component={Launches}/>
+            <Route exact path='/launch/:flight_number' component={Launch}/>
+          </div>
+        </Router>
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
